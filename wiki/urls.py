b4c3995251bc6c6e4da5bde3d7186dd79d10ef22
Sudha Maurya <sudha@Sudhas-MacBook-Pro.local>
2024-02-22 14:50:31 +0530

    refactored get wiki page sections api and used cache

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from wiki.views import get_wiki_page
+from wiki.views import get_wiki_sections, status
 
 urlpatterns = [
-    path("page/", get_wiki_page, name="get_wiki_page"),
+    path("page/", get_wiki_sections, name="get_wiki_sections")
 ]
--- wiki/views.py
+++ wiki/views.py
@@ -7,28 +7,43 @@ from rest_framework.response import Response
 from wiki.constants import SUCCESS, ERROR, DATA
 
 
+page_cache = {}
+
+
+def get_wiki_page(page_title):
+    if not page_cache.get(page_title):
+        wiki_wiki = wikipediaapi.Wikipedia('Wiki summary', 'en')
+        wiki_page = wiki_wiki.page(page_title)
+        if not wiki_page.exists():
+            return
+
+        page_cache[wiki_page.title] = wiki_page
+        return wiki_page
+
+    return page_cache[page_title]
+
+
 @api_view(["GET"])
-def get_wiki_page(request):
-    search_text = request.GET.get("title")
-    if not search_text:
-        response = {SUCCESS: False, ERROR: "Invalid page title"}
+def get_wiki_sections(request):
+    page_title = request.GET.get("page_title")
+    if not page_title:
+        response = {SUCCESS: False, ERROR: "Invalid page title."}
         return Response(response, status=status.HTTP_400_BAD_REQUEST, content_type="application/json")
 
-    wiki_wiki = wikipediaapi.Wikipedia('Wiki summary', 'en')
-    page_py = wiki_wiki.page(search_text)
-    if not page_py.exists():
-        response = {SUCCESS: False, ERROR: "Could not find wiki page with the given search text."}
+    wiki_page = get_wiki_page(page_title)
+    if not wiki_page:
+        response = {SUCCESS: False, ERROR: "This page does not exists."}
         return Response(response, status=status.HTTP_400_BAD_REQUEST, content_type="application/json")
 
     section_titles = []
-    for section in page_py.sections:
+    for section in wiki_page.sections:
         section_titles.append(section.title)
 
     response = {
         SUCCESS: True,
         DATA: {
-            "title": page_py.title,
-            "full_url": page_py.fullurl,
+            "page_title": wiki_page.title,
+            "full_url": wiki_page.fullurl,
             "sections": section_titles
         }
     }
