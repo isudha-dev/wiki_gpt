4e6f671ed6b43f714f84731d21d872f19b037809
Sudha Maurya <sudha@Sudhas-MacBook-Pro.local>
2024-02-23 13:17:02 +0530

    update setting to read from env and disable debug on prod

--- wiki_gpt/settings.py
+++ wiki_gpt/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -18,12 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ez75tivbb@(5_pn+j%@ns^gc$$zhg=f+ojz&wdgs24bok22iza'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("CURR_ENV") == "dev"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
